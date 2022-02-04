ec07a84a5a98ec55c592dd22ed6a6af589847088
iFranki3 <99048947+iFranki3@users.noreply.github.com>
2022-02-04 16:28:22 -0500

    Add files via upload

--- assignment1_part1.py
+++ assignment1_part1.py
@@ -1,1 +1,47 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def listDivide(numbers, divide = 2):
+    divisibleCount = 0
+    for everyNum in numbers:
+        if everyNum % divide == 0:
+            divisibleCount += 1
+  
+    return divisibleCount
+
+class ListDivideException(Exception):
+    pass
+
+def testListDivide():
+    try:
+        if not listDivide([1,2,3,4,5]) == 2:
+            raise ListDivideException
+        if not listDivide([2,4,6,8,10]) == 5:
+            raise ListDivideException
+        if not listDivide([30,54,63,98,100], divide = 10) == 2:
+            raise ListDivideException
+        if not listDivide([]) == 0:
+            raise ListDivideException
+        if not listDivide([1,2,3,4,5],1) == 5:
+            raise ListDivideException
+        
+    except ListDivideException:
+        print("Division exception occured")
+
+testListDivide()
+
+
+# In[2]:
+
+
+print (testListDivide())
+
+
+# In[ ]:
+
+
+
 
---
+++ assignment1_part2.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[32]:
+
+
+class Book:   
+    author = "" 
+    title = ""
+
+    def __init__(self, Author, Title):  
+        self.author = Author
+        self.title = Title
+
+    def display(self):
+        print("\""+self.title+", written by "+self.author+"\"")
+
+object1 = Book("John Steinbeck", "Of Mice and Men")
+object1.display()
+
+object2 = Book("Harper Lee", "To Kill a Mockingbird")
+object2.display()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
